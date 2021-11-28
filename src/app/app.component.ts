46199091f60bf29567e68321dabbd543114b6c99
profile4vaibhav <mail4vaibhavgadge@gmail.com>
2021-11-28 21:06:11 +0530

    Reactibve Forms - Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
+  signupForm: FormGroup;
+
+  ngOnInit() {
+    this.initializeSignupForm();
+  }
+
+  initializeSignupForm() {
+    this.signupForm = new FormGroup({
+      'uname': new FormControl(null, Validators.required),
+      'uemail': new FormControl(null, [Validators.required, Validators.email]),
+      'ugender': new FormControl('male')
+    })
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
 }
